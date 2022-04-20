25a281f6d0eb03fa36dac4644fbbe8aa743def10
csnpch <ChitsanuphongChaihong@outlook.com>
2022-04-21 02:05:32 +0700

    2.0.1

--- app/configs/env.js
+++ app/configs/env.js
@@ -6,7 +6,7 @@ const server = {
 }
 
 const database = {
-    uri: process.env.MONGODB_URI || "mongodb://localhost:27017/App",
+    uri: process.env.MONGODB_URI || "mongodb://localhost:27017/BrainPlz",
 }
 
 const authentication = {
